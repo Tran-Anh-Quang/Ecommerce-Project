d8b6e768118ce241d58ccf40f9a8067b0f53ab23
dinos <quangta1@newwave.vn>
2024-01-12 17:27:35 +0700

    Completed Admin Feature

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/ecommerce/admin/security/EcommerceUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.ecommerce.admin.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.ecommerce.admin.user.UserRepository;
+import com.ecommerce.common.entity.User;
+
+public class EcommerceUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepository.getUserByEmail(email);
+		if (user != null) {
+			return new EcommerceUserDetails(user);
+		}
+		throw new UsernameNotFoundException("Could not find any user with email " + email);
+	}
+
+}
---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/ecommerce/admin/security/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.ecommerce.admin.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		return new EcommerceUserDetailsService();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
+	}
+	
+
+	@Bean
+	SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
+
+		http.authenticationProvider(authenticationProvider());
+
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+				.formLogin(form -> form
+						.loginPage("/login")
+						.usernameParameter("email")
+						.permitAll());
+
+		return http.build();
+	}
+	
+	@Bean
+	WebSecurityCustomizer configureWebSecurity() throws Exception {
+		return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+	}
+
+}
