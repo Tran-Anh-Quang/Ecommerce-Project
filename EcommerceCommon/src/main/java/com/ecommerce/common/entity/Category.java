487e644d4b123df630a46d463b2da5663b9b156c
dinos <quangta1@newwave.vn>
2024-01-15 15:16:58 +0700

    Completed Test CategoryRepository Module

---
+++ EcommerceCommon/src/main/java/com/ecommerce/common/entity/Category.java
@@ -0,0 +1,53 @@
+package com.ecommerce.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 64, nullable = false, unique = true)
+    private String alias;
+
+    @Column(length = 128, nullable = false)
+    private String image;
+
+    private boolean enabled;
+
+    @OneToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> children = new HashSet<>();
+
+    public Category(String name) {
+        this.name = name;
+        this.alias = name;
+        this.image = "default.png";
+    }
+
+    public Category(String name, Category parent) {
+        this(name);
+        this.parent = parent;
+    }
+
+    public Category(Integer id) {
+        this.id = id;
+    }
+}
---
+++ EcommerceWebParent/EcommerceBackEnd/src/test/java/com/ecommerce/admin/user/CategoryRepositoryTests.java
@@ -0,0 +1,85 @@
+package com.ecommerce.admin.user;
+
+import com.ecommerce.admin.category.repository.CategoryRepository;
+import com.ecommerce.common.entity.Category;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(showSql = false)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class CategoryRepositoryTests {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void testCreateRootCategory(){
+        Category category = new Category("Computers");
+        Category savedCategory = categoryRepository.save(category);
+
+        assertThat(savedCategory.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testCreateSubCategory(){
+        Category parent = new Category(2);
+        Category subCategory = new Category("Gaming Laptops", parent);
+        Category savedCategory = categoryRepository.save(subCategory);
+
+        assertThat(savedCategory.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testGetCategory(){
+        Category category = categoryRepository.findById(1).get();
+        System.out.println(category.getName());
+
+        Set<Category> children = category.getChildren();
+
+        for (Category subCategory : children){
+            System.out.println(subCategory.getName());
+        }
+
+        assertThat(children.size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testPrintHierarchicalCategories(){
+        Iterable<Category> categories = categoryRepository.findAll();
+
+        for (Category category : categories){
+            if (category.getParent() == null){
+                System.out.println(category.getName());
+
+                Set<Category> children = category.getChildren();
+
+                for (Category subCategory : children){
+                    System.out.println("---" + subCategory.getName());
+                    printChildren(subCategory, 1);
+                }
+            }
+        }
+    }
+
+    public void printChildren(Category parent, int subLevel){
+
+        int newSubLevel = subLevel + 1;
+
+        Set<Category> children = parent.getChildren();
+        for (Category subCategory : children){
+            for (int i = 0; i < newSubLevel; i++){
+                System.out.println("---");
+            }
+            System.out.println(subCategory.getName());
+
+            printChildren(subCategory, newSubLevel);
+        }
+    }
+}
