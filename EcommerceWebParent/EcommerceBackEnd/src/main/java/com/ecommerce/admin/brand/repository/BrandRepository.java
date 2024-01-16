e54a1930405c09941ba46295ab653b7f29fc58eb
dinos <quangta1@newwave.vn>
2024-01-16 16:31:24 +0700

    Completed Unit Test BrandRepository

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/ecommerce/admin/brand/repository/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.admin.brand.repository;
+
+import com.ecommerce.common.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+}
