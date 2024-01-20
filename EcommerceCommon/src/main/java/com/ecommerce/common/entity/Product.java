5eb09cb9063d81c5e44317d24f6e9559921b80e7
dinos <quangta1@newwave.vn>
2024-01-20 13:10:18 +0700

    Completed Upload Product Images Unit Testing

--- EcommerceCommon/src/main/java/com/ecommerce/common/entity/Product.java
+++ EcommerceCommon/src/main/java/com/ecommerce/common/entity/Product.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -56,6 +58,9 @@ public class Product {
 
     private float weight;
 
+    @Column(name = "main_image", nullable = false)
+    private String mainImage;
+
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
@@ -64,10 +69,17 @@ public class Product {
     @JoinColumn(name = "brand_id")
     private Brand brand;
 
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private Set<ProductImage> images = new HashSet<>();
+
     @Override
     public String toString() {
         return "Product" +
                 "id=" + id +
                 ", name='" + name + '\'';
+    }
+
+    public void addExtraImage(String imageName){
+        this.images.add(new ProductImage(imageName, this));
     }
 }
